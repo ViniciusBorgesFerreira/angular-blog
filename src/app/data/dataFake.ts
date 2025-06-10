410f9be9e4a0570d92d05766c4a489c2c5e1ac66
ViniciusBorgesFerreira <tec.viniciusferreira@gmail.com>
2025-06-10 12:50:04 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: '1',
+    photoCover:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic1.srcdn.com%2Fwordpress%2Fwp-content%2Fuploads%2F2022%2F07%2FThe-Avengers-Earth---s-Mightiest-Heroes-Poster.jpg&f=1&nofb=1&ipt=5198cd84a2c91f105883e72fa6a72a075705b115b856a6db0d0e356d1261ac88',
+    title: 'saiu novo filme',
+    description: 'saiu o filme e turuoplay tchuplacktunai',
+  },
+  {
+    id: '2',
+    photoCover:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic1.srcdn.com%2Fwordpress%2Fwp-content%2Fuploads%2F2019%2F02%2FMarvel-Netflix-Featured-Cropped-1.jpg&f=1&nofb=1&ipt=35698c63e054c3f57998136a13bcd0314c5e0315f692a51cc772e6333b218e27',
+    title: 'Nova série anunciada pela Disney+',
+    description: 'saiu serie e turuoplay tchuplacktunai',
+  },
+  {
+    id: '3',
+    photoCover:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftrecobox.com.br%2Fwp-content%2Fuploads%2F2023%2F06%2Ftrecobox.com.br-saiba-quem-e-david-corenswet-o-novo-superman-dos-cinemas-superman-david-corenswet.png&f=1&nofb=1&ipt=df121d1cb634b265b445a35dc73574a690ff4a57231d74cf6f75f9a33f196b72',
+    title: 'Novo filme do Pantera Negra anunciado na plataforma Netflix',
+    description: 'saiu o filme e turuoplay tchuplacktunai',
+  },
+  {
+    id: '4',
+    photoCover:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.infinitynews.com.br%2Fwp-content%2Fuploads%2F2019%2F08%2Fjoker-2019-movie-4k-g0-1920x1080.jpg&f=1&nofb=1&ipt=9aeae8759bb274c2cf28acb9e06f7fa2ad96667800c92bdc8a32fde3047c0633',
+    title: 'Continuação deste clássico já tem data para estreia',
+    description: 'saiu o filme e turuoplay tchuplacktunai',
+  },
+];
